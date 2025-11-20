383b81f20611b1c57b9867b804e03842f8eb0dd7
kozha <kozha@WIN-QKC3MJARA7J>
2025-11-20 23:27:14 +0300

    Добавьте файлы проекта.

---
+++ Tumakov/Classes/ACipher.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+using Tumakov.Interfaces;
+
+namespace Tumakov.Classes
+{
+    internal class ACipher : ICipher
+    {
+        private static char[] _upperAlphabet = {
+            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й',
+            'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
+            'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'
+        };
+        private static char[] _lowerAlphabet = {
+            'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й',
+            'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+            'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'
+        };
+
+        public ACipher()
+        {
+
+        }
+
+        public string Encode(string text)
+        {
+            int indexOfSymb;
+            char[] charText;
+            charText = text.ToCharArray();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsUpper(charText[i]))
+                {
+                    indexOfSymb = Array.IndexOf(_upperAlphabet, charText[i]);
+
+                    if (indexOfSymb == 32)
+                    {
+                        charText[i] = _upperAlphabet[0];
+                    }
+                    else
+                    {
+                        charText[i] = _upperAlphabet[indexOfSymb + 1];
+                    }
+                }
+                else if (char.IsLower(charText[i]))
+                {
+                    indexOfSymb = Array.IndexOf(_lowerAlphabet, charText[i]);
+
+                    if (indexOfSymb == 32)
+                    {
+                        charText[i] = _lowerAlphabet[0];
+                    }
+                    else
+                    {
+                        charText[i] = _lowerAlphabet[indexOfSymb + 1];
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return new string(charText);
+        }
+
+        public string Decode(string text)
+        {
+            int indexOfSymb;
+            char[] charText = text.ToCharArray();
+
+            for (int i = 0; i <  charText.Length; i++)
+            {
+                if (char.IsUpper(charText[i]))
+                {
+                    indexOfSymb = Array.IndexOf(_upperAlphabet, charText[i]);
+
+                    if (indexOfSymb == 0)
+                    {
+                        charText[i] = _upperAlphabet[32];
+                    }
+                    else
+                    {
+                        charText[i] = _upperAlphabet[indexOfSymb - 1];
+                    }
+                }
+                else if (char.IsLower(charText[i]))
+                {
+                    indexOfSymb = Array.IndexOf(_lowerAlphabet, charText[i]);
+
+                    if (indexOfSymb == 0)
+                    {
+                        charText[i] = _lowerAlphabet[32];
+                    }
+                    else
+                    {
+                        charText[i] = _lowerAlphabet[indexOfSymb - 1];
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            return new string(charText);
+        }
+    }
+}
---
+++ Tumakov/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Tumakov.Classes;
+
+namespace Tumakov
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Упражнение 10.1
+            Console.WriteLine("Упражнение 10.1");
+
+            ACipher aCipher = new ACipher();
+
+            Console.WriteLine(aCipher.Encode("Привет я 1"));
+            Console.WriteLine(aCipher.Decode("Рсйгёу а 1"));
+
+
+            BCipher bCipher = new BCipher();
+
+            Console.WriteLine(bCipher.Encode("Привет! 1 а я в"));
+            Console.WriteLine(bCipher.Decode("Поцэъм! 1 я а э"));
+        }
+    }
+}
